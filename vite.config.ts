56d8d62a44bcb3a1e8679cb28de9d0babf9c12a7
yeonwan < >
2025-05-01 21:57:51 -0700

    fix: add base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
+  base: "/wemake/", // GitHub Pages base URL
 });
