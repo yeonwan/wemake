78c089f5199d46102d520425aabc4c5319546619
yeonwan <43770449+yeonwan@users.noreply.github.com>
2025-05-02 18:43:02 +0000

    Deploying to gh-pages from @ yeonwan/wemake@4b99cf3f00c9ef2bbf2b6d819344c83ba757d293 🚀

--- assets/my-profile-page-BxP1IGP2.js
+++
@@ -1,1 +0,0 @@
-import"./jsx-runtime-CEMuh2-_.js";import"./index-l2PZgWEW.js";const i=()=>[{title:"My Profile | WeMake"},{name:"description",content:"View and edit your profile"}];export{i as meta};
